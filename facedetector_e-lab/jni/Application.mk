99bf4751c23e1e249157d3817b5fa54a1bed8f1d
Soumith Chintala <soumith@gmail.com>
2013-12-12 17:44:53 -0500

    r9b fixes

--- facedetector_e-lab/jni/Application.mk
+++ facedetector_e-lab/jni/Application.mk
@@ -1,5 +1,6 @@
 # Build only ARMv7-A machine code.
-APP_ABI := armeabi armeabi-v7a
+APP_ABI := armeabi-v7a
 APP_STL :=gnustl_static
 LOCAL_ARM_NEON := true
-APP_OPTIM := debug
+APP_CFLAGS += -fopenmp -Wno-error=format-security
+APP_LDFLAGS += -fopenmp
