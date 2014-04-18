cfe998fe88f297abd322496baf2639039af44227
Soumith Chintala <soumith@gmail.com>
2014-04-18 19:49:16 -0400

    making changes for split torch

--- src/cmake/TorchWrap.cmake
+++ src/cmake/TorchWrap.cmake
@@ -4,7 +4,7 @@ MACRO(ADD_TORCH_WRAP target luafile)
   SET(cfile "${_file_}.c")
   ADD_CUSTOM_COMMAND(
     OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${cfile}"
-    COMMAND torch-lua ARGS "-e \"dofile('${Torch_SOURCE_PKG}/wrap/init.lua')\" " "${CMAKE_CURRENT_SOURCE_DIR}/${luafile}" "${CMAKE_CURRENT_BINARY_DIR}/${cfile}"
+    COMMAND luajit ARGS "-e \"dofile('${Torch_SOURCE_PKG}/wrap/init.lua')\" " "${CMAKE_CURRENT_SOURCE_DIR}/${luafile}" "${CMAKE_CURRENT_BINARY_DIR}/${cfile}"
     DEPENDS "${luafile}" ${Torch_SOURCE_LUA} "${Torch_SOURCE_PKG}/wrap/init.lua" "${Torch_SOURCE_PKG}/wrap/types.lua")
   ADD_CUSTOM_TARGET(${target} DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/${cfile}")
 ENDMACRO(ADD_TORCH_WRAP)
