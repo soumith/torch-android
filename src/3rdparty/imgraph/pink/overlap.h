4a48234060a1bc65022de8d24dbff67d2e9acc98
Soumith Chintala <soumith@fb.com>
2016-01-29 11:04:07 -0500

    removed src

--- src/3rdparty/imgraph/pink/overlap.h
+++
@@ -1,3 +0,0 @@
-float * Overlap(struct xvimage * segment, struct xvimage *mask,  int nb_classes);
-float * Overlap1(struct xvimage *segment, struct xvimage *mask, int nb_classes);
-int * DecisionSegmentation(float * Segments, int rs, int cs, int nb_segments, float *f, int nb_classes, float t1, float t2, float t3);
--- src/3rdparty/nn/Tanh.lua
+++
@@ -1,9 +0,0 @@
-local Tanh = torch.class('nn.Tanh', 'nn.Module')
-
-function Tanh:updateOutput(input)
-   return input.nn.Tanh_updateOutput(self, input)
-end
-
-function Tanh:updateGradInput(input, gradOutput)
-   return input.nn.Tanh_updateGradInput(self, input, gradOutput)
-end
