0c00c49d3be2922b338eea4edf04f11bb4d15c1a
Boris Fomitchev <bfomitchev@nvidia.com>
2016-02-25 19:28:22 -0800

    Refactoring, Cutorch made working

--- demos/android-demo/jni/Android.mk
+++ demos/android-demo/jni/Android.mk
@@ -1,13 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := torchdemo
 
 LOCAL_C_INCLUDES += ../../install/include
 
-LOCAL_SRC_FILES := torchandroid.cpp torchdemo.cpp android_fopen.c
+LOCAL_SRC_FILES := torchdemo.cpp
 
-LOCAL_LDLIBS := -llog -landroid -L ../../install/lib -L ../../install/libs/armeabi-v7a  -lluaT -lluajit -lTH  -lTHNN -lnn  -ltorch -lnnx -limage -ltorchandroid -lluaT -lluajit -lTH -lTHNN -lnn  -ltorch -lnnx -limage -ltorchandroid
+LOCAL_LDLIBS := -L ../../install/lib -L ../../install/libs/armeabi-v7a -lcunn -lTHCUNN -lcutorch -lnnx -limage  -lnn -lTHNN -ltorch  -lTH -lluaT -lluajit -ltorchandroid -llog -landroid
 
 include $(BUILD_SHARED_LIBRARY)
--- demos/android-demo/jni/Application.mk
+++ demos/android-demo/jni/Application.mk
@@ -1,5 +1,4 @@
 # Build only ARMv7-A machine code.
 APP_ABI := armeabi-v7a
-APP_STL :=gnustl_static
 APP_CFLAGS += -fopenmp
 APP_LDFLAGS += -fopenmp
