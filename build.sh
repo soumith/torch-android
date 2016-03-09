eff519ea3c69e406df34afb9593b4ba4f917fb1d
Boris Fomitchev <bfomitchev@nvidia.com>
2016-03-08 19:23:29 -0800

    Added libjpeg

--- build.sh
+++ build.sh
@@ -4,18 +4,26 @@
 NDKABI=21
 NDKVER=toolchains/arm-linux-androideabi-4.9
 
-NVCC=`which nvcc`
-APP_ABI="armeabi-v7a with NEON"
-M_ARCH="-march=armv7-a"
-ABI_NAME=armv7-linux-androideabi
-COMPUTE_NAME=Kepler-M
+ARCH=${ARCH:-"v7n"}
 
-# Uncomment for ARM64
-# APP_ABI=arm64-v8a
-# M_ARCH=-march=arm8-a
-# ABI_NAME=aarch64-linux-androideabi"
-# COMPUTE_NAME=Maxwell
+if [[ "$ARCH" == "v8" ]]; then
+    APP_ABI=arm64-v8a
+    M_ARCH=-march=arm8-a
+    ABI_NAME=aarch64-linux-androideabi
+    COMPUTE_NAME=Maxwell-M
+elif [[ "$ARCH" == "v7n" ]]; then
+    APP_ABI="armeabi-v7a with NEON"
+    M_ARCH="-march=armv7-a"
+    ABI_NAME=armv7-linux-androideabi
+    COMPUTE_NAME=Kepler-M
+elif [[ "$ARCH" == "v7" ]]; then
+    APP_ABI="armeabi-v7a"
+    M_ARCH="-march=armv7-a"
+    ABI_NAME=armv7-linux-androideabi
+    COMPUTE_NAME=Kepler
+fi
 
+    NVCC=`which nvcc`
 export MAKE=make
 export MAKEARGS=-j$(getconf _NPROCESSORS_ONLN)
 
@@ -53,16 +61,6 @@ export INSTALL_DIR=$SCRIPT_ROOT_DIR/install
 echo "INSTALL_DIR=${INSTALL_DIR}"
 set +e # hard errors
 export CMAKE_INSTALL_SUBDIR="share/cmake/torch"
-
-cd $SCRIPT_ROOT_DIR
-
-cd distro/extra/FindCUDA && \
-    (cmake -E make_directory build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" -DCMAKE_INSTALL_SUBDIR="${CMAKE_INSTALL_SUBDIR}" && make install) \
-    && echo "FindCuda installed" || exit 1
-
-cd $SCRIPT_ROOT_DIR
-# Build host luajit for minilua and buildvm
-cd distro/exe/luajit-rocks/luajit-2.1
 NDK=$ANDROID_NDK
 NDKVER=$NDK/$NDKVER
 if [[ "$unamestr" == 'Linux' ]]; then
@@ -74,15 +72,8 @@ NDK_SYSROOT=$NDK/platforms/android-$NDKABI/arch-arm
 NDKF="--sysroot $NDK_SYSROOT"
 NDKARCH="$M_ARCH -mfloat-abi=softfp -Wl,--fix-cortex-a8"
 
-# make clean
-$MAKE $MAKEARGS HOST_CC="gcc -m32" CC="gcc" HOST_SYS=$unamestr TARGET_SYS=Linux CROSS=$NDKP TARGET_FLAGS="$NDKF $NDKARCH"
-
-cd $SCRIPT_ROOT_DIR
-
-mkdir -p build
-cd build
-
-cmake .. -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_TOOLCHAIN_FILE=$SCRIPT_ROOT_DIR/cmake/android.toolchain.cmake \
+do_cmake_config() {
+cmake $1 -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_TOOLCHAIN_FILE=$SCRIPT_ROOT_DIR/cmake/android.toolchain.cmake \
     -DANDROID_NDK=${ANDROID_NDK} -DANDROID_ABI="${APP_ABI}" \
     -DCUDA_ARCH_NAME=${COMPUTE_NAME} \
     -DWITH_CUDA=ON -DWITH_LUAROCKS=OFF -DWITH_LUAJIT21=ON\
@@ -92,15 +83,38 @@ cmake .. -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_TOOLCHAIN_FILE=$SCRIPT_ROOT_DIR/cma
     -DLIBRARY_OUTPUT_PATH_ROOT="${INSTALL_DIR}" \
     -DLUAJIT_SYSTEM_MINILUA=$SCRIPT_ROOT_DIR/distro/exe/luajit-rocks/luajit-2.1/src/host/minilua \
     -DLUAJIT_SYSTEM_BUILDVM=$SCRIPT_ROOT_DIR/distro/exe/luajit-rocks/luajit-2.1/src/host/buildvm \
-    -DCMAKE_C_FLAGS="-DDISABLE_POSIX_MEMALIGN" \
+    -DCMAKE_C_FLAGS="-DDISABLE_POSIX_MEMALIGN"
+echo " -------------- Configuring DONE ---------------"
+}
 
-echo " -------------- Configuring DONE ---------------"  \
+cd $SCRIPT_ROOT_DIR
 
+cd distro/extra/FindCUDA && \
+    (cmake -E make_directory build && cd build && do_cmake_config .. && make install) \
+    && echo "FindCuda installed" || exit 1
+
+cd $SCRIPT_ROOT_DIR
+
+cd external && \
+    (cmake -E make_directory build && cd build && do_cmake_config .. && make install) \
+    && echo "externals installed" || exit 1
+5B
+cd $SCRIPT_ROOT_DIR
+# Build host luajit for minilua and buildvm
+cd distro/exe/luajit-rocks/luajit-2.1
+
+# make clean
+$MAKE $MAKEARGS HOST_CC="gcc -m32" CC="gcc" HOST_SYS=$unamestr TARGET_SYS=Linux CROSS=$NDKP TARGET_FLAGS="$NDKF $NDKARCH"
 
 echo "Done installing Lua"
 
+cd $SCRIPT_ROOT_DIR
+(cmake -E make_directory build && cd build && do_cmake_config ..) || exit 1
+
+cd build
 
 (cd distro/exe && $MAKE $MAKEARGS install) || exit 1
+# cwrap needs to be there first
 (cd distro/pkg/cwrap && $MAKE $MAKEARGS install) || exit 1
 (cd distro/pkg && $MAKE $MAKEARGS install) || exit 1
 
