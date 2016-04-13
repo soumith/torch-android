acca502b8b9b7c4fddd2edbaa5583c12f53a433d
Boris Fomitchev <bfomitchev@nvidia.com>
2016-04-13 01:18:12 -0700

    Adding missed Torch.java

---
+++ demos/android-demo/src/com/torch/Torch.java
@@ -0,0 +1,28 @@
+package com.torch;
+
+import android.util.Log;
+import android.content.res.AssetManager;
+import android.content.pm.ApplicationInfo;
+import android.content.Context;
+
+public class Torch
+{
+    AssetManager    assetManager;
+    ApplicationInfo info;
+
+    // native method
+    private native String  jni_call(AssetManager manager, String path, String luafile);
+
+    public Torch(Context myContext) {
+	assetManager = myContext.getAssets();
+        info = myContext.getApplicationInfo();
+        System.loadLibrary("torchandroid");
+	Log.d("Torch","Torch() called\n");
+    }
+
+    public String call(String lua) {
+	Log.d("Torch.call(%s)\n", lua);
+	return jni_call(assetManager, info.nativeLibraryDir, lua);
+    }
+
+}
